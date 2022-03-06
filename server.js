818be7cd38229355c19715ea722ab104be15493b
Cafe Au Lait Server <coffeehousecomics416@gmail.com>
2022-03-06 23:13:22 +0000

    Add story and comic end point for testing

--- server.js
+++ server.js
@@ -10,4 +10,13 @@ app.post('/', function (req, res, next) {
     res.status(200).send("Hello!");
 });
 
+// Get request for story/comic end points (For testing)
+app.get('/story', function(req, res, next) {
+    res.status(200).send("Reached story endpoint");
+});
+
+app.get('/comic', function(req, res, next) {
+    res.status(200).send("Reached Comic endpoint");
+});
+
 app.listen(port, () => console.log(`Warmup app listening on port ${port}!`));
