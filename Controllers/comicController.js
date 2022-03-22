14f1003ce094826a9c0fa31527ac584bfa5c4fe8
Shaan Khan <shaan.khan@stonybrook.edu>
2022-03-22 01:05:08 -0400

    Move the req/res stuff of comics to the controller.

--- Controllers/comicController.js
+++ Controllers/comicController.js
@@ -1,102 +1,331 @@
 const ComicController = {};
 
 ComicController.explore = async function (req, res) {
+    /* Explore ------------
+        Request body: {
+            sortMethod: String
+            searchCriteria: String
+        }
 
+        Response {
+            status 200 OK or 500 ERROR
+            body: {
+                [Comic Series Objects]
+            }
+        }
+    */
 }
 
 ComicController.subscriptions = async function (req, res) {
+    /* Subscriptions ------------
+        Request body: {
+            sortMethod: String
+            searchCriteria: String
+        }
 
+        Response {
+            status 200 OK or 500 ERROR
+            body: {
+                [Comic Series Objects]
+            }
+        }
+    */
 }
 
 ComicController.getProfileById = async function (req, res) {
+    /* Get profile By ID ------------
+        Request body: {}
 
+        Response {
+            status 200 OK or 500 ERROR
+            body: {
+                id: ObjectId
+                displayName: String,
+                bio: String,
+                profileImage: Image,
+            }
+        }
+    */
 }
 
 ComicController.getProfileByUserName = async function (req, res) {
+    /* Get profile By userName ------------
+        Request body: {}
 
+        Response {
+            status 200 OK or 500 ERROR
+            body: {
+                id: ObjectId
+                displayName: String,
+                bio: String,
+                profileImage: Image,
+            }
+        }
+    */
 }
 
-
-// ComicController.profileByUserName = async function (req, res) {
-
-// }
 
 // Need Authentication ------------------------------------------------
 
 // Creating
 ComicController.create = async function (req, res) {
+    /* Create a Comic (On the backend) ------------
+        Request body: {
+            id: String || null,
+            userName: String  || null
+        }
 
+        Response {
+            status: 200 OK or 500 ERROR,
+            body: {
+                id: ObjectId
+            }
+        }
+    */
 }
 
 ComicController.published = async function (req, res) {
+    /* Get published comic by id  ------------
+        Request body: {}
 
+        Response {
+            status: 200 OK or 500 ERROR,
+            body: {
+                name: String,
+                description: String,
+
+                author: String,
+                authorID: ObjectId,
+
+                series: {
+                    isSeriesMember: Boolean,
+                    seriesName: String,
+                    seriesID: Number
+                },
+
+                publishedDate: Date,
+                beans: Number,
+                comments: [CommentSchema],
+                pages: [
+                    {
+                        title: String,
+                        image: Image,
+                    }
+                ]
+            }
+        }
+    */
 }
 
 ComicController.unpublished = async function (req, res) {
+    /* Get UNpublished comic by id  ------------
+        Request body: {}
 
+        Response {
+            status: 200 OK or 500 ERROR,
+            body: {
+                name: String,
+                description: String,
+                
+                author: String,
+                authorID: ObjectId,
+
+                pages: [
+                    {
+                        title: String,
+                        konvaPage: JSON
+                    }
+                ]
+
+                prefabs: [ JSON ],
+                stickers: [ JSON ]
+            }
+        }
+    */
 }
 
 // Publishing
 ComicController.publish = async function (req, res) {
+    /* Publish Comic ------------ 
+        Request body: {
+            series: {
+                isSeriesMember: Boolean,
+                seriesName: String,
+            },
 
+            Response {
+                status 200 OK or 500 ERROR
+            }
+        }
+    */
 }
 
 // Deleting
 ComicController.delete = async function (req, res) {
+    /* Deleting a comic ------------
+        Request body: {}
 
+        Response {
+            status: 200 OK or 500 ERROR,
+        }
+    */
 }
 
 ComicController.delete_comment = async function (req, res) {
+    /* Delete a comment------------
+        Request body: {}
 
+        Response {
+            status: 200 OK or 500 ERROR,
+        }
+    */
 }
 
 ComicController.delete_forumPost = async function (req, res) {
+    /* Delete a Forum Post------------
+        Request body: {}
+    
+        Response {
+            status: 200 OK or 500 ERROR,
+        }
+    */
+}
 
+ComicController.deleteSticker = async function (req, res) {
+    /* Delete a sticker ------------
+        Request body: {}
+
+        Response {
+            status: 200 OK or 500 ERROR
+        }
+    */
 }
 
 // User related Content
 ComicController.user_saved = async function (req, res) {
+    /* Get user's saved comics ------------
+        Request body: {}
 
+        Response {
+            status: 200 OK or 500 ERROR,
+        }
+    */
 }
 
 ComicController.user_toggleForum = async function (req, res) {
-
+    /* Toggle Forum for user ------------
+        Request body: {}
+    
+        Response {
+            status: 200 OK or 500 ERROR
+        }
+    */
 }
 
 // Comic metadata editing (Cover photo, Title, Bio, Series)
 ComicController.metadata_update = async function (req, res) {
+    /* Update the metadata of a comic ------------
+        Request body: {
+            title: String,
+            bio: String
+        }
 
+        Response {
+            status: 200 OK or 500 ERROR
+        }
+    */
 }
 
 // Comic content editing
 ComicController.content_save = async function (req, res) {
+    /* Update the metadata of a comic ------------
+        Request body: {
+            title: String,
+            bio: String
+        }
 
+        Response {
+            status: 200 OK or 500 ERROR
+        }
+    */
 }
 
 ComicController.content_saveSticker = async function (req, res) {
+    /* Save a Sticker ------------
+        Request body: {
+            title: String,
+            bio: String
+        }
 
+        Response {
+            status: 200 OK or 500 ERROR,
+            body: {
+                id: ObjectId
+            }
+        }
+    */
 }
 
 // Commenting
 ComicController.comment = async function (req, res) {
+    /* Comment on a Comic ------------
+        Request body: {
+            text: String
+        }
 
+        Response {
+            status: 200 OK or 500 ERROR,
+        }
+    */
 }
 
 ComicController.comment_forumPost = async function (req, res) {
-
+    /* Comment on a Forum Post ------------
+        Request body: {
+            text: String
+        }
+    
+        Response {
+            status: 200 OK or 500 ERROR,
+        }
+    */
 }
 
 // Voting (upvoting/downvoting AKA liking/disliking)
 ComicController.vote = async function (req, res) {
-
+    /* Vote on a Comic ------------
+        Request body: {
+            isUpvote: Boolean
+        }
+    
+        Response {
+            status: 200 OK or 500 ERROR,
+        }
+    */
 }
 
 ComicController.vote_forumPost = async function (req, res) {
-
+    /* Vote on a Forum Post ------------
+        Request body: {
+            isUpvote: Boolean
+        }
+    
+        Response {
+            status: 200 OK or 500 ERROR,
+        }
+    */
 }
 
 ComicController.vote_comment = async function (req, res) {
-
+    /* Vote on a Comment ------------
+        Request body: {
+            isUpvote: Boolean
+        }
+    
+        Response {
+            status: 200 OK or 500 ERROR,
+        }
+    */
 }
 
 
--- Routes/comicRouter.js
+++ Routes/comicRouter.js
@@ -2,296 +2,46 @@ const express = require('express')
 const router = express.Router()
 const ComicController = require('../Controllers/comicController')
 
-/* Explore ------------
-    Request body: {
-        sortMethod: String
-        searchCriteria: String
-    }
 
-    Response {
-        status 200 OK or 500 ERROR
-        body: {
-            [Comic Series Objects]
-        }
-    }
-*/
 router.get('/explore', ComicController.explore);
-/* Subscriptions ------------
-    Request body: {
-        sortMethod: String
-        searchCriteria: String
-    }
-
-    Response {
-        status 200 OK or 500 ERROR
-        body: {
-            [Comic Series Objects]
-        }
-    }
-*/
 router.get('/subscriptions', ComicController.subscriptions);
 
-/* Get profile By ID ------------
-    Request body: {}
-
-    Response {
-        status 200 OK or 500 ERROR
-        body: {
-            id: ObjectId
-            displayName: String,
-            bio: String,
-            profileImage: Image,
-        }
-    }
-*/
 router.get('/profile/:id', ComicController.getProfileById);
-/* Get profile By userName ------------
-    Request body: {}
-
-    Response {
-        status 200 OK or 500 ERROR
-        body: {
-            id: ObjectId
-            displayName: String,
-            bio: String,
-            profileImage: Image,
-        }
-    }
-*/
 router.get('/profile/:userName', ComicController.getProfileByUserName);
 
 
 // Creating ------------------------------------------------
-/* Create a Comic (On the backend) ------------
-    Request body: {
-        id: String || null,
-        userName: String  || null
-    }
-
-    Response {
-        status: 200 OK or 500 ERROR,
-        body: {
-            id: ObjectId
-        }
-    }
-*/
 router.post('/create', ComicController.create);
-/* Get published comic by id  ------------
-    Request body: {}
-
-    Response {
-        status: 200 OK or 500 ERROR,
-        body: {
-            name: String,
-            description: String,
-
-            author: String,
-            authorID: ObjectId,
-
-            series: {
-                isSeriesMember: Boolean,
-                seriesName: String,
-                seriesID: Number
-            },
-
-            publishedDate: Date,
-            beans: Number,
-            comments: [CommentSchema],
-            pages: [
-                {
-                    title: String,
-                    image: Image,
-                }
-            ]
-        }
-    }
-*/
 router.get('/published/:id', ComicController.published);
-/* Get UNpublished comic by id  ------------
-    Request body: {}
-
-    Response {
-        status: 200 OK or 500 ERROR,
-        body: {
-            name: String,
-            description: String,
-            
-            author: String,
-            authorID: ObjectId,
-
-            pages: [
-                {
-                    title: String,
-                    konvaPage: JSON
-                }
-            ]
-
-            prefabs: [ JSON ],
-            stickers: [ JSON ]
-        }
-    }
-*/
 router.get('/unpublished/:id', ComicController.unpublished);
 
-
 // Publishing
-/* Publish Comic ------------ 
-    Request body: {
-        series: {
-            isSeriesMember: Boolean,
-            seriesName: String,
-        },
-
-        Response {
-            status 200 OK or 500 ERROR
-        }
-    }
-*/
 router.post('/publish/:id', ComicController.publish);
 
-/* Deleting a comic ------------
-    Request body: {}
-
-    Response {
-        status: 200 OK or 500 ERROR,
-    }
-*/
+// Deleting
 router.delete('/:id', ComicController.delete);
-/* Delete a comment------------
-    Request body: {}
-
-    Response {
-        status: 200 OK or 500 ERROR,
-    }
-*/
 router.delete('/comment/:id', ComicController.delete_comment);
-/* Delete a Forum Post------------
-    Request body: {}
-
-    Response {
-        status: 200 OK or 500 ERROR,
-    }
-*/
 router.delete('/forumPost/:id', ComicController.delete_forumPost);
-/* Delete a sticker ------------
-    Request body: {}
-
-    Response {
-        status: 200 OK or 500 ERROR
-    }
-*/
-router.delete('/sticker/:id', ComicController.sticker);
+router.delete('/sticker/:id', ComicController.deleteSticker);
 
 // User related Content
-/* Get user's saved comics ------------
-    Request body: {}
-
-    Response {
-        status: 200 OK or 500 ERROR,
-    }
-*/
 router.get('/user/saved', ComicController.user_saved);
-/* Toggle Forum for user ------------
-    Request body: {}
-
-    Response {
-        status: 200 OK or 500 ERROR
-    }
-*/
 router.post('/user/toggleForum', ComicController.user_toggleForum);
 
 // Comic metadata editing (Cover photo, Title, Bio, Series)
-/* Update the metadata of a comic ------------
-    Request body: {
-        title: String,
-        bio: String
-    }
-
-    Response {
-        status: 200 OK or 500 ERROR
-    }
-*/
 router.put('/metadata/update/:id', ComicController.metadata_update);
 
 // Comic content editing
-/* Update the metadata of a comic ------------
-    Request body: {
-        title: String,
-        bio: String
-    }
-
-    Response {
-        status: 200 OK or 500 ERROR
-    }
-*/
 router.put('/content/save', ComicController.content_save);
-/* Save a Sticker ------------
-    Request body: {
-        title: String,
-        bio: String
-    }
-
-    Response {
-        status: 200 OK or 500 ERROR,
-        body: {
-            id: ObjectId
-        }
-    }
-*/
 router.post('/content/saveSticker', ComicController.content_saveSticker);
 
 // Commenting
-/* Comment on a Comic ------------
-    Request body: {
-        text: String
-    }
-
-    Response {
-        status: 200 OK or 500 ERROR,
-    }
-*/
 router.post('/comment/:id', ComicController.comment);
-/* Comment on a Forum Post ------------
-    Request body: {
-        text: String
-    }
-
-    Response {
-        status: 200 OK or 500 ERROR,
-    }
-*/
 router.post('/comment/forumPost/:id', ComicController.comment_forumPost);
 
 // Voting (upvoting/downvoting AKA liking/disliking)
-/* Vote on a Comic ------------
-    Request body: {
-        isUpvote: Boolean
-    }
-
-    Response {
-        status: 200 OK or 500 ERROR,
-    }
-*/
 router.post('/vote/:id', ComicController.vote);
-/* Vote on a Forum Post ------------
-    Request body: {
-        isUpvote: Boolean
-    }
-
-    Response {
-        status: 200 OK or 500 ERROR,
-    }
-*/
 router.post('/vote/forumPost', ComicController.vote_forumPost);
-/* Vote on a Comment ------------
-    Request body: {
-        isUpvote: Boolean
-    }
-
-    Response {
-        status: 200 OK or 500 ERROR,
-    }
-*/
 router.post('/vote/comment', ComicController.vote_comment);
 
 module.exports = router
