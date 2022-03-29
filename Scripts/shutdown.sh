04f773e117bb77a9b2c5b7a8676291d5e17c31c1
Shaan Khan <shaan.khan@stonybrook.edu>
2022-03-28 20:08:41 -0400

    Adjust shutdown script to use npm kill-port

--- Scripts/shutdown.sh
+++ Scripts/shutdown.sh
@@ -1,5 +1,6 @@
 echo \"Stopping the server...\"
-pid=$(lsof -i:3000 -t); kill -TERM $pid || kill -KILL $pid
+# pid=$(lsof -i:3000 -t); kill -TERM $pid || kill -KILL $pid
+npx kill-port 3000
 echo \"Clear content of log file\"
 cat /dev/null > output.log
 echo \"DONE\"
